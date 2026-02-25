b1862b8de18290e90431c90782e0c489d4f4173e
Guilherme <guilhermesousa1804@gmail.com>
2026-02-24 21:39:06 -0300

    projeto criado

---
+++ silva/src/test/java/com/example/silva/SilvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.silva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
